98fa9d26dfdac96d2f3ef4efabc85fa19275f474
young-zy <youngzhaozhou@gmail.com>
2018-09-03 15:14:26 +0800

    9-3 fixed first login page shows without css add parse json file add side bar feature misc things

--- app.py
+++ app.py
@@ -3,23 +3,28 @@ from flask import render_template
 from flask import request
 from flask import session
 from flask import redirect
+from flask import g
+import os
 from flask import escape
 
 import json
 
 app = Flask(__name__)
-app.secret_key = "ADDQWEDFAFAEAED"
-
-app.f = open("data.json", encoding="utf-8")
-app.data = json.load(app.f)
+app.secret_key = "dashfbdkfbasdkfvnmasdfnc"
 
 
 @app.before_request
-def process_request():
+def before_request():
+    g.f = open("data.json", encoding="utf-8")
+    g.data = json.load(g.f)
+    g.dict_arr = g.data['list']
+    g.total_num = len(g.dict_arr)
+    g.id_list = []
+    for i in range(1, g.total_num+1):
+        g.id_list.append(i)
+
     if request.path == "/login":
         return None
-    if not session.get("user_info"):
-        return redirect("/login")
     return None
 
 
@@ -38,23 +43,32 @@ def login():
         username = request.form.get('username')
         session["username"] = username
         session["number"] = 1
-        return redirect("/answer/1")
+        session["total_num"] = g.total_num
+        return redirect('/answer/1')
 
 
 @app.route('/logout')
 def logout():
-    # remove the username from the session if it's there
     session.pop('username', None)
+    session.pop('number', None)
     return redirect('/login')
 
 
 @app.route('/answer/<int:number>', methods=['GET', 'POST'])
 def answer(number):
     if request.method == "GET":
-        return render_template('answer.html', number=number)
+        if 'username' in session:
+            return render_template('answer.html', number=number, total_num=g.total_num, id_list=g.id_list)
+        else:
+            return redirect('/login')
     else:
-        session['%d' % int(request.form.get('number'))] = request.form.get('group1')
-        number = number + 1
+        tmp = request.form.get('group1')
+        if tmp is not None:
+            session['%d' % int(request.form.get('number'))] = tmp
+            number = number + 1
+        else:
+            if number is not 1:
+                number = number - 1
         session['number'] = number
         return redirect('/answer/%d' % number)
 
